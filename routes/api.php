64e662cc5c430265c990ca3fef6f193e17d5a3e3
pkron <le_tombeau_de_couperin08@yahoo.co.jp>
2024-09-23 23:52:52 +0900

    ルーティングベース

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,11 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('customers', 'App\Http\Controllers\CustomerController');
+// adminロールのみがアクセス可能なルートを定義する場合
+// Route::middleware(['auth', 'role:admin'])->group(function () {
+//     Route::get('/admin', function () {
+//         // Admin画面の処理
+//     });
+// });
+
+Route::apiResource('customer', 'App\Http\Controllers\CustomerController');
